bccac4de2da908c534997e6a59123f57f78691ff
Philipotieno <tmitchellb007@gmail.com>
2020-01-07 18:10:21 +0300

    feat: patch drinks

--- backend/src/api.py
+++ backend/src/api.py
@@ -65,139 +65,82 @@ def post_drinks(payload):
         }), 201
     except:
         abort(422)
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 
+@app.route('/drinks/<id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def patch_drinks(payload, id):
+    drink = Drink.query.filter_by(id=id).first()
+    if not drink:
+        abort(404)
+        
+    try:
+        data = request.get_json()['title'] and request.get_json()['recipe']
+        if not data:
+            abort(400)
+    except (TypeError, KeyError):
+        abort(400)
+    
+    # check if drink title exists
+    if Drink.query.filter_by(title=request.get_json()['title']).first():
+        abort(409)
 
-'''
-@TODO implement endpoint
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-'''
-'''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-'''
-'''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-'''
-'''
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+    try:
+        if request.get_json().get('title'):
+            drink.title=request.get_json()['title']
 
-
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
-
-
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
-
-
-## Error Handling
-'''
-Example error handling for unprocessable entity
-'''
-# @app.errorhandler(422)
-# def unprocessable(error):
-#     return jsonify({
-#                     "success": False, 
-#                     "error": 422,
-#                     "message": "unprocessable"
-#                     }), 422
-
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above 
-'''
-
+        if request.get_json().get('recipe'):
+            drink.recipe=json.dumps(request.get_json()['recipe'])
+        drink.update()
+        return jsonify({
+            'success': True,
+            'drinks': [drink.long()]
+        }), 200
+    except Exception:
+        abort(422)
 
 
 # Error Handling
 
-# @app.errorhandler(400)
-# def bad_request(error):
-#     return jsonify({
-#         "success": False,
-#         "error": 400,
-#         "message": "bad request"
-#     }), 400
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "Bad request"
+    }), 400
 
 
-# @app.errorhandler(404)
-# def not_found(error):
-#     return jsonify({
-#         "success": False,
-#         "error": 404,
-#         "message": "not found"
-#     }), 404
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "Resource not found"
+    }), 404
 
 
-# @app.errorhandler(409)
-# def duplicate(error):
-#     return jsonify({
-#         "success": False,
-#         "error": 409,
-#         "message": "duplicate"
-#     }), 409
+@app.errorhandler(409)
+def duplicate(error):
+    return jsonify({
+        "success": False,
+        "error": 409,
+        "message": "Title already exists"
+    }), 409
 
 
-# # @app.errorhandler(422)
-# # def unprocessable(error):
-# #     return jsonify({
-# #         "success": False,
-# #         "error": 422,
-# #         "message": "unprocessable"
-# #     }), 422
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "Unprocessable"
+    }), 422
 
 
-# @app.errorhandler(AuthError)
-# def auth_error(error):
-#     return jsonify({
-#         "success": False,
-#         "error": error.status_code,
-#         "message": error.error
-#     }), error.status_code
+@app.errorhandler(AuthError)
+def auth_error(error):
+    return jsonify({
+        "success": False,
+        "error": error.status_code,
+        "message": error.error
+    }), error.status_code
