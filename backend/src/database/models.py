2b1ed71f80440b8ebe7341a66f8a5c59620a4746
Philipotieno <tmitchellb007@gmail.com>
2020-01-07 20:06:41 +0300

    refactor: add way to return recipe

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -48,7 +48,8 @@ class Drink(db.Model):
     '''
     def short(self):
         print(json.loads(self.recipe))
-        short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
+        r = json.loads(self.recipe)
+        short_recipe = [{'color': r['color'], 'parts': r['parts']}]
         return {
             'id': self.id,
             'title': self.title,
